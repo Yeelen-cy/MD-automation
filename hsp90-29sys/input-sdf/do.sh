b5d76a73a8dc9c7628469649e8b0dde9cc8a3b8b
zzysimm <153146715+zzysimm@users.noreply.github.com>
2025-03-17 16:52:07 +0800

    Delete hsp90-29sys/input-sdf directory

--- hsp90-29sys/input-sdf/do.sh
+++
@@ -1,8 +0,0 @@
-for a in `awk '{print $1}' index`
-do 
-b=`grep $a index|awk '{print $2}'`
-
-~/software/Schrodinger_Suites_2021-2/ligprep -R e -epik -ph 7.4 -pht 0.0 -isd ../wget-ligand-expo/${b}_model.sdf -osd ${a}-${b}.sdf
-
-
-done
